ad43cc15fff3a2faacba961cdc1d0707a0e3df25
azrayaal <azrayazidalkautsar@gmail.com>
2024-10-27 17:59:43 +0700

    update api notifications

--- src/api/notifications/controller.ts
+++ src/api/notifications/controller.ts
@@ -6,7 +6,7 @@ export const getAllNotifications = async (req: Request, res: Response) => {
   try {
     const userId = req.user?.id;
     const [data] = await pool.query(
-      "SELECT * FROM notifications WHERE userId = ?",
+      "SELECT * FROM notifications WHERE userId = ? and is_removed = FALSE order by id desc",
       [userId]
     );
     res.json({ success: true, data });
@@ -28,6 +28,28 @@ export const getDetailNotifications = async (req: Request, res: Response) => {
       id,
     ]);
     res.json({ success: true, data });
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching notifications", error });
+  }
+};
+
+export const removeNotifications = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    // const [data] = await pool.query(
+    //   "SELECT * FROM notifications WHERE id = ?",
+    //   [id]
+    // );
+    // await pool.query("UPDATE notifications SET is_read = TRUE WHERE id = ?", [
+    //   id,
+    // ]);
+
+    await pool.query<ResultSetHeader>(
+      "UPDATE notifications SET is_removed = TRUE WHERE id = ?",
+      [id]
+    );
+
+    res.json({ success: true, message: "Notifications removed successfully" });
   } catch (error) {
     res.status(500).json({ message: "Error fetching notifications", error });
   }
--- src/api/notifications/router.ts
+++ src/api/notifications/router.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getAllNotifications, getDetailNotifications } from "./controller";
+import { getAllNotifications, getDetailNotifications, removeNotifications } from "./controller";
 import { isLoginUser } from "../../middleware/auth";
 
 const router = Router();
 
 router.get("/", isLoginUser, getAllNotifications);
-router.get("/:id", getDetailNotifications);
+router.get("/:id", isLoginUser, getDetailNotifications);
+router.post("/remove/:id", isLoginUser, removeNotifications);
 
 export default router;
